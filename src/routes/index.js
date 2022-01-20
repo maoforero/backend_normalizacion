e80fa40180223bf03459a5d70794cbd911d34b05
maoforero <maforeror@gmail.com>
2022-01-20 01:31:39 -0500

    correccion api routes get

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const routerProducts = require('./products')
 
+router.get("/", (req, res) => {
+  res.send('api')
+})
+
 router.use('/products', routerProducts);
 
 module.exports = router;
