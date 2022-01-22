6725302d09811b455388853817c29e6aa70a8cc2
maoforero <maforeror@gmail.com>
2022-01-21 23:13:35 -0500

    controller products update funcional

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -1,7 +1,8 @@
+const Data =  require('../../../list.json');
 const controller = {};
 
 controller.index = (req, res) => {
-  res.send("products");
+  res.send(Data);
 };
 
 controller.post = (req, res) => {
@@ -9,7 +10,23 @@ controller.post = (req, res) => {
   res.json({
     msg: 'objeto creado',
     data: 'gg'
-  })
+  });
+}
+
+controller.put = (req, res) => {
+  const {idItem} = req.params;
+  const item = Data.find((item) => item.id === idItem);
+  if(item){
+    const {id, name, price} = req.body;
+    item.id = id;
+    item.name = name;
+    item.price = price;
+    res.json({
+      msg: `Item ${id} update`
+    });
+    return;
+  }
+  res.json({msg:`Item Unknow`})
 }
 
 module.exports = controller
--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/products')
 
-router.get('/', controller.index)
+router.get('/jaja', controller.index)
 
 router.post('/', controller.post)
 
-
+router.put('/update/:id', controller.put)
 
 module.exports = router
